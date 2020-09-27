c019a97f9d734905144ba4b7b3091110a785f0bb
Kunal Gaurav <kunalgaurav18@gmail.com>
2020-09-27 23:21:33 +0530

    Message change rectified

--- src/TodolistApp.js
+++ src/TodolistApp.js
@@ -5,6 +5,7 @@ function TodolistApp() {
   return (
     <div style={{ width: "70%", margin: "20px auto", textAlign: "center" }}>
       <h3>Todo List</h3>
+      <p><b>Note:</b> Data will be lost on page refresh.</p>
       <Actions />
     </div>
   );
